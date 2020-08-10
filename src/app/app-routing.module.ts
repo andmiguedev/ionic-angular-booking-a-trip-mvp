8024dc2b259de7926d75fc830c5d124386d250e8
Miguel Gomez <mgomez.developer@gmail.com>
2020-08-10 15:15:06 -0400

    Refactor routing configuration to use Tab navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,13 @@ import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: "home",
-    loadChildren: () =>
-      import("./home/home.module").then((m) => m.HomePageModule),
+    path: "",
+    redirectTo: "pages",
+    pathMatch: "full",
   },
   {
-    path: "",
-    redirectTo: "save-modal",
-    pathMatch: "full",
+    path: "pages",
+    loadChildren: "./pages/pages.module#PagesPageModule",
   },
   {
     path: "welcome",
@@ -18,86 +17,9 @@ const routes: Routes = [
       import("./pages/welcome/welcome.module").then((m) => m.WelcomePageModule),
   },
   {
-    path: "select-place",
-    loadChildren: () =>
-      import("./pages/places/select-place/select-place.module").then(
-        (m) => m.SelectPlacePageModule
-      ),
-  },
-  {
-    path: "offer-places",
-    loadChildren: () =>
-      import("./pages/places/offer-places/offer-places.module").then(
-        (m) => m.OfferPlacesPageModule
-      ),
-  },
-  {
-    path: "nearby-places",
-    loadChildren: () =>
-      import("./pages/places/nearby-places/nearby-places.module").then(
-        (m) => m.NearbyPlacesPageModule
-      ),
-  },
-  {
-    path: "create-trip",
-    loadChildren: () =>
-      import("./pages/trips/create-trip/create-trip.module").then(
-        (m) => m.CreateTripPageModule
-      ),
-  },
-  {
-    path: "edit-trip",
-    loadChildren: () =>
-      import("./pages/trips/edit-trip/edit-trip.module").then(
-        (m) => m.EditTripPageModule
-      ),
-  },
-  {
-    path: "detail-trip",
-    loadChildren: () =>
-      import("./pages/trips/detail-trip/detail-trip.module").then(
-        (m) => m.DetailTripPageModule
-      ),
-  },
-  {
-    path: "user-trips",
-    loadChildren: () =>
-      import("./pages/trips/user-trips/user-trips.module").then(
-        (m) => m.UserTripsPageModule
-      ),
-  },
-  {
     path: "support",
     loadChildren: () =>
       import("./pages/support/support.module").then((m) => m.SupportPageModule),
-  },
-  {
-    path: "share-modal",
-    loadChildren: () =>
-      import("./pages/modals/share-modal/share-modal.module").then(
-        (m) => m.ShareModalPageModule
-      ),
-  },
-  {
-    path: "recent-places",
-    loadChildren: () =>
-      import("./pages/places/recent-places/recent-places.module").then(
-        (m) => m.RecentPlacesPageModule
-      ),
-  },
-  {
-    path: "success-modal",
-    loadChildren: () =>
-      import("./pages/modals/success-modal/success-modal.module").then(
-        (m) => m.SuccessModalPageModule
-      ),
-  },
-  {
-    path: "save-modal",
-    loadChildren: () =>
-      import("./pages/modals/save-modal/save-modal.module").then(
-        (m) => m.SaveModalPageModule
-      ),
   },
 ];
 
---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,68 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+import { PagesPage } from "./pages.page";
+
+const routes: Routes = [
+  {
+    path: "tabs",
+    component: PagesPage,
+
+    children: [
+      {
+        path: "places",
+        children: [
+          {
+            path: "",
+            loadChildren: "./places/places.module#PlacesPageModule",
+          },
+          {
+            path: ":pageId",
+            loadChildren:
+              "./places/recent-places.module#RecentPlacesPageModule",
+          },
+        ],
+      },
+      {
+        path: "trips",
+        children: [
+          {
+            path: "",
+            loadChildren: "./trips/trips.module#TripsPageModule",
+          },
+          {
+            path: "create",
+            loadChildren:
+              "./trips/create-trip/create-trip.module#CreateTripPageModule",
+          },
+          {
+            path: "edit/:pageId",
+            loadChildren:
+              "./trips/edit-trip/edit-trip.module#EditTripPageModule",
+          },
+          {
+            path: ":pageId",
+            loadChildren:
+              "./trips/detail-trip/detail-trip.module#DetailTripPageModule",
+          },
+        ],
+      },
+      {
+        path: "",
+        redirectTo: "/pages/tabs/places",
+        pathMatch: "full",
+      },
+    ],
+  },
+  {
+    path: "",
+    redirectTo: "/pages/tabs/places",
+    pathMatch: "full",
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PagesRoutingModule {}
