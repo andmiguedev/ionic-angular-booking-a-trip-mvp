630dd425dc557ebf2437355a6a99ba54fcb0aaf9
Miguel Gomez <mgomez.developer@gmail.com>
2020-08-12 17:14:49 -0400

    Structure local rendering service into class entities

--- src/app/pages/users/user-profile/user-profile.page.ts
+++ src/app/pages/users/user-profile/user-profile.page.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { OnlineProfile } from "./../../../models/online-profile.model";
+import { DisplayVisitorProfiles } from "src/app/data/visitor-profiles";
+
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.page.html',
   styleUrls: ['./user-profile.page.scss'],
 })
 export class UserProfilePage implements OnInit {
+  showVisitorProfile: OnlineProfile[];
 
-  constructor() { }
+  constructor(private renderLocalData: DisplayVisitorProfiles) { }
 
   ngOnInit() {
+    this.showVisitorProfile = this.renderLocalData.getVisitorProfile;
   }
 
 }
