c6266f2ca23554c565b5594b72c43f449695a8b3
Miguel Gomez <mgomez.developer@gmail.com>
2020-08-14 09:23:53 -0400

    Add authentication service that guards non public pages

--- src/app/pages/users/user-profile/user-profile.module.ts
+++ src/app/pages/users/user-profile/user-profile.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from "@ionic/angular";
 
-import { UserProfilePageRoutingModule } from './user-profile-routing.module';
+import { UserProfilePageRoutingModule } from "./user-profile-routing.module";
 
-import { UserProfilePage } from './user-profile.page';
+import { UserProfilePage } from "./user-profile.page";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    UserProfilePageRoutingModule
+    UserProfilePageRoutingModule,
   ],
-  declarations: [UserProfilePage]
+  declarations: [UserProfilePage],
 })
 export class UserProfilePageModule {}
